0b3ced651d53fe2f9318df685327d2cb089bd8a1
kenny <ekeneeze3@gmail.com>
2018-07-11 14:30:38 +0100

    implement handlesubmit to clear text

--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -8,13 +8,18 @@ class ChatScreen extends StatefulWidget {
 
 class ChatScreenState extends State<ChatScreen> {
 
+  final TextEditingController _chatController = new TextEditingController();
+  void _handleSubmit(String text){
+    _chatController.clear();
+  }
+
   Widget _chatEnvironment (){
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal:8.0),
       child: new TextField(
-        decoration: new InputDecoration.collapsed(
-          hintText: "Start typing ..."
-        ),
+        decoration: new InputDecoration.collapsed(hintText: "Start typing ..."),
+        controller: _chatController,
+        onSubmitted: _handleSubmit,
       ),
     );
   }
